c0b020048062fad7ccfd81d065aa024f4c457902
Leon <59918677+leonlol-dev@users.noreply.github.com>
2022-08-18 12:47:44 +0100

    powerup sounds & homing missile wip

--- Assets/Scripts/PowerUps/PowerUp.cs
+++ Assets/Scripts/PowerUps/PowerUp.cs
@@ -5,12 +5,33 @@ using UnityEngine;
 public class PowerUp : MonoBehaviour
 {
     public PowerUpEffect powerUpEffect;
+    public AudioClip[] powerUpSounds;
+
+    private SpriteRenderer rend;
+    private AudioSource powerUpAudio;
+    
+
+    private void Start()
+    {
+        //Get Components
+        rend = GetComponent<SpriteRenderer>();
+        powerUpAudio = GetComponent<AudioSource>();
+
+        //Choose a random sound.
+        powerUpAudio.clip = powerUpSounds[Random.Range(0, powerUpSounds.Length)];
+
+    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+   
+        //Play the clip.
+        powerUpAudio.Play();
+
         if(collision.gameObject.tag == "Player")
         {
-            Destroy(gameObject);
+            rend.enabled = false;
+            Destroy(gameObject, 0.75f);
             powerUpEffect.Apply(collision.gameObject);
         }
       
