2d3399dbb7ac5a135846a73a6df5bb9220f9af59
Leon <59918677+leonlol-dev@users.noreply.github.com>
2022-09-18 22:46:25 +0100

    asset fix
    asdasd

--- Assets/ObjectPooling.cs
+++ Assets/ObjectPooling.cs
@@ -7,53 +7,18 @@ public class ObjectPooling : MonoBehaviour
 {
 
     public static ObjectPooling sharedInstance;
-    public List<GameObject> pooledObjects;
-    public GameObject objectToPool;
-    public int amountToPool;
 
-    public bool pooled = false;
 
-    // Start is called before the first frame update
-    void Start()
+
+    private void Awake()
     {
-    
-
-    }
-
-    public void Update()
-    {
-        if (!pooled)
+        if (sharedInstance == null)
         {
-            pooled = true;
-            sharedInstance = GetComponent<ObjectPooling>();
-
-            pooledObjects = new List<GameObject>();
-            GameObject tmp;
-            for (int i = 0; i < amountToPool; i++)
-            {
-                tmp = Instantiate(gameObject);
-                tmp.SetActive(false);
-                pooledObjects.Add(tmp);
-            }
+            sharedInstance = this;
         }
     }
 
-    public GameObject GetPooledObject()
-    {
-        for (int i = 0; i < amountToPool; i++)
-        {
-            if (!pooledObjects[i].activeInHierarchy)
-            {
-                return pooledObjects[i];
-            }
-        }
-        return null;
-    }
 
-    public void SetPooledObject(GameObject _objectToPool)
-    {
-        objectToPool = _objectToPool;
-    }
-    
+
 
 }
